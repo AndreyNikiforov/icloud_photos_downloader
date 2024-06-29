4713d98177b3a8a43a3ebe270e62ff22766ada40
Andrey Nikiforov <andy.nikiforov@gmail.com>
2024-06-29 11:57:51 -0700

    fix test

--- src/icloudpd/base.py
+++ src/icloudpd/base.py
@@ -4,7 +4,6 @@
 from multiprocessing import freeze_support  # fmt: skip
 freeze_support()  # fmt: skip  # fixing tqdm on macos
 
-from __future__ import print_function
 import typing
 
 from icloudpd.counter import Counter
